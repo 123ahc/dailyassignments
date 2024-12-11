cd372ecf39047177206ee599591627bffb11a249
123ahc <ADAM.CONNELL4271@STU.MTEC.EDU>
2024-12-10 18:24:55 -0700

    Add files via upload
    Completed assignment for unit JS, day 1, part 3

--- JS.1.3/index.js
+++ JS.1.3/index.js
@@ -4,7 +4,9 @@
 let yardWidth = 100;
 let yardLength = 15;
 
-let yardPerimeter;
+let yardPerimeter = (yardWidth * 2) + (yardLength * 2);
+
+console.log(yardPerimeter);
 
 
 // 2) With the yardPerimeter variable go ahead and use back ticks (``)
@@ -14,7 +16,9 @@ let yardPerimeter;
 // Don't remember how to do this? Go back to the first repl.it assignment from today
 // and look at the example problems console.log().
 
-let messagePerimeter;
+let messagePerimeter = `You are going to need ${yardPerimeter}ft of fence`;
+
+console.log(messagePerimeter);
 
 // 3)One last thing. It is going to cost me $15 per foot of fence.
 // What is the total price I am going to have to pay?
@@ -23,4 +27,6 @@ let messagePerimeter;
 
 let pricePerFoot = 15;
 
-let totalPrice;
+let totalPrice = yardPerimeter * pricePerFoot;
+
+console.log(totalPrice);
